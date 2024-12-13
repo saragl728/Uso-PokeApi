38c2e9df1a4fdfab32f120891139da031787f3f1
saragl728 <saragl728@gmail.com>
2024-12-13 18:54:06 +0100

    Ligero cambio en la búsqueda de datos de un Pokémon

--- escript2.js
+++ escript2.js
@@ -4,6 +4,8 @@ var btn = document.getElementById("car");
 var tabula = document.getElementById("cTabla");
 var texto = document.getElementById("buscador");
 
+var temp = ""; //variable que almacena el nombre del último pokémon buscado
+
 btn.addEventListener("click", muestraDatos);
 
 function getUrl() {
@@ -11,73 +13,77 @@ function getUrl() {
 }
 
 function muestraDatos() {
-  tabula.innerHTML = "";
-
   fetch(getUrl())
     .then((datos) => datos.json())
     .then((datos2) => {
-      fila = document.createElement("tr");
-      t1 = document.createElement("td");
-      t2 = document.createElement("td");
-      t3 = document.createElement("td");
-      t4 = document.createElement("td");
-      t5 = document.createElement("td");
-      t6 = document.createElement("td");
-      t7 = document.createElement("td");
-      t8 = document.createElement("td");
-      t9 = document.createElement("td");
-      t10 = document.createElement("td");
-      t11 = document.createElement("td");
+      //si se busca el mismo pokémon, no se repite el proceso de recreación de la tabla
+      if (temp != datos2.name) {
+        tabula.innerHTML = "";
+        temp = datos2.name;
 
-      a1 = document.createTextNode(datos2.id);
-      t1.appendChild(a1);
+        fila = document.createElement("tr");
+        t1 = document.createElement("td");
+        t2 = document.createElement("td");
+        t3 = document.createElement("td");
+        t4 = document.createElement("td");
+        t5 = document.createElement("td");
+        t6 = document.createElement("td");
+        t7 = document.createElement("td");
+        t8 = document.createElement("td");
+        t9 = document.createElement("td");
+        t10 = document.createElement("td");
+        t11 = document.createElement("td");
 
-      a2 = document.createTextNode(datos2.name);
-      t2.appendChild(a2);
+        a1 = document.createTextNode(datos2.id);
+        t1.appendChild(a1);
 
-      imag = document.createElement("img");
-      imag.setAttribute("src", `${datos2.sprites.front_default}`);
-      t3.appendChild(imag);
+        a2 = document.createTextNode(datos2.name);
+        t2.appendChild(a2);
 
-      a4 = document.createTextNode(datos2.stats[0].base_stat);
-      t4.appendChild(a4);
+        imag = document.createElement("img");
+        imag.setAttribute("src", `${datos2.sprites.front_default}`);
+        t3.appendChild(imag);
 
-      a5 = document.createTextNode(datos2.stats[1].base_stat);
-      t5.appendChild(a5);
+        a4 = document.createTextNode(datos2.stats[0].base_stat);
+        t4.appendChild(a4);
 
-      a6 = document.createTextNode(datos2.stats[2].base_stat);
-      t6.appendChild(a6);
+        a5 = document.createTextNode(datos2.stats[1].base_stat);
+        t5.appendChild(a5);
 
-      a7 = document.createTextNode(datos2.stats[3].base_stat);
-      t7.appendChild(a7);
+        a6 = document.createTextNode(datos2.stats[2].base_stat);
+        t6.appendChild(a6);
 
-      a8 = document.createTextNode(datos2.stats[4].base_stat);
-      t8.appendChild(a8);
+        a7 = document.createTextNode(datos2.stats[3].base_stat);
+        t7.appendChild(a7);
 
-      a9 = document.createTextNode(datos2.stats[5].base_stat);
-      t9.appendChild(a9);
+        a8 = document.createTextNode(datos2.stats[4].base_stat);
+        t8.appendChild(a8);
 
-      //tipos
-      a10 = document.createTextNode(datos2.types[0].type.name);
-      t10.appendChild(a10);
+        a9 = document.createTextNode(datos2.stats[5].base_stat);
+        t9.appendChild(a9);
 
-      a11 =
-        datos2.types.length > 1
-          ? document.createTextNode(datos2.types[1].type.name)
-          : document.createTextNode("");
-      t11.appendChild(a11);
+        //tipos
+        a10 = document.createTextNode(datos2.types[0].type.name);
+        t10.appendChild(a10);
 
-      fila.appendChild(t1);
-      fila.appendChild(t2);
-      fila.appendChild(t3);
-      fila.appendChild(t4);
-      fila.appendChild(t5);
-      fila.appendChild(t6);
-      fila.appendChild(t7);
-      fila.appendChild(t8);
-      fila.appendChild(t9);
-      fila.appendChild(t10);
-      fila.appendChild(t11);
-      tabula.appendChild(fila);
+        a11 =
+          datos2.types.length > 1
+            ? document.createTextNode(datos2.types[1].type.name)
+            : document.createTextNode("");
+        t11.appendChild(a11);
+
+        fila.appendChild(t1);
+        fila.appendChild(t2);
+        fila.appendChild(t3);
+        fila.appendChild(t4);
+        fila.appendChild(t5);
+        fila.appendChild(t6);
+        fila.appendChild(t7);
+        fila.appendChild(t8);
+        fila.appendChild(t9);
+        fila.appendChild(t10);
+        fila.appendChild(t11);
+        tabula.appendChild(fila);
+      }
     });
 }
